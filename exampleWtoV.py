16f035dd5d8b9df23459ec9d540b1fc97931ad28
mathrho-u018850 <zhenyoung.li@gmail.com>
2015-04-02 11:27:22 +0200

    modify wod2vec

--- exampleWtoV.py
+++ exampleWtoV.py
@@ -24,13 +24,15 @@ def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     #pretrained_model = './vectors.bin'
-    pretrained_model = '../freebase-vectors-skipgram1000-en.bin'
-    #pretrained_model = '../GoogleNews-vectors-negative300.bin'
+    #pretrained_model = '../freebase-vectors-skipgram1000-en.bin'
+    pretrained_model = '../GoogleNews-vectors-negative300.bin'
     model = gensim.models.Word2Vec.load_word2vec_format(pretrained_model, binary=True)
     #model['animal']
     #print model.similarity('/en/dog', '/en/cat')
-    print model.similarity('/en/man', '/en/woman')
-    print model.similarity('/en/man', '/en/male')
+    #print model.similarity('/en/man', '/en/woman')
+    #print model.similarity('/en/man', '/en/male')
+    print model.similarity('man', 'woman')
+    print model.similarity('man', 'male')
 
 
 if __name__ == "__main__":
