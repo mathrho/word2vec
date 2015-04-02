31d26cefd8f3d246437511c2b0852051d68cb2c8
mathrho-u018850 <zhenyoung.li@gmail.com>
2015-04-02 11:10:26 +0200

    modify wod2vec

---
+++ exampleWtoV.py
@@ -0,0 +1,37 @@
+#/datastore/zhenyang/bin/python
+
+import gensim, logging
+import sys
+import os
+from xml.etree import ElementTree
+
+def get_parentmap(tree):
+    parent_map = {}
+    for p in tree.iter():
+        for c in p:
+            if c in parent_map:
+                parent_map[c].append(p)
+                # Or raise, if you don't want to allow this.
+            else:
+                parent_map[c] = [p]
+                # Or parent_map[c] = p if you don't want to allow this
+    return parent_map
+
+
+def main():
+
+    ##############
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    #pretrained_model = './vectors.bin'
+    pretrained_model = '../freebase-vectors-skipgram1000-en.bin'
+    #pretrained_model = '../GoogleNews-vectors-negative300.bin'
+    model = gensim.models.Word2Vec.load_word2vec_format(pretrained_model, binary=True)
+    #model['animal']
+    #print model.similarity('/en/dog', '/en/cat')
+    print model.similarity('/en/man', '/en/woman')
+    print model.similarity('/en/man', '/en/male')
+
+
+if __name__ == "__main__":
+    main()
