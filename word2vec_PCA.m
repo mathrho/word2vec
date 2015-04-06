e0ce582e5e0bfe2b7ff9110989e8f63bc02e0c73
Zhenyang <mathrho@vpn-staff-146-50-68-190.vpn.uva.nl>
2015-04-06 22:57:30 +0200

    mod extractVecMat

--- word2vec_PCA.m
+++ word2vec_PCA.m
@@ -10,13 +10,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 load word2vec_GoogleNews_PCAmap30.mat
 
-load synset_word2vec_GoogleNews.mat
-%load attr_word2vec_GoogleNews.mat
+%load synset_word2vec_GoogleNews.mat
+load attr_word2vec_GoogleNews.mat
 
 word2vec = word2vec';
 word2vec = normalizeColsL2(double(word2vec));
 word2vec = pcamap' * (word2vec - repmat(meanvec, 1, size(word2vec, 2)));
 word2vec = word2vec';
 
-save('synset_word2vec_GoogleNews_PCA30.mat', 'word2vec');
-%save('attr_word2vec_GoogleNews_PCA30.mat', 'word2vec');
+%save('synset_word2vec_GoogleNews_PCA30.mat', 'word2vec');
+save('attr_word2vec_GoogleNews_PCA30.mat', 'word2vec');
