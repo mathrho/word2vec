7b2ff983d66380ed7e77b305c5eb8720d6eba717
Zhenyang <mathrho@vpn-staff-146-50-68-190.vpn.uva.nl>
2015-04-06 19:38:33 +0200

    mod extractVecMat

--- extractVecMat.py
+++ extractVecMat.py
@@ -85,7 +85,7 @@ def main():
             break
 
         #print dir(model)
-        if idc:
+        if False:
             classnames = target.get('words').split(', ')
             for classname in classnames:
                 namewords = classname.split(' ')
