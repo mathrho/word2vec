6bab8c192fd811ea98758c800a2a83cebc037e07
Zhenyang <mathrho@vpn-staff-146-50-68-190.vpn.uva.nl>
2015-04-06 22:55:31 +0200

    mod extractVecMat

--- word2vec_PCA.m
+++ word2vec_PCA.m
@@ -1,22 +1,22 @@
-load word2vec_GoogleNews.mat
+%load word2vec_GoogleNews.mat
+%word2vec = word2vec';
+
+%word2vec = normalizeColsL2(double(word2vec));
+%nPCADims = 30;                                                                                                                                   
+%[X, pcamap] = netpca(double(word2vec'), nPCADims);
+%meanvec = mean(word2vec, 2);
+%save('word2vec_GoogleNews_PCAmap30.mat', 'pcamap', 'meanvec');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+load word2vec_GoogleNews_PCAmap30.mat
+
+load synset_word2vec_GoogleNews.mat
+%load attr_word2vec_GoogleNews.mat
+
+word2vec = word2vec';
+word2vec = normalizeColsL2(double(word2vec));
+word2vec = pcamap' * (word2vec - repmat(meanvec, 1, size(word2vec, 2)));
 word2vec = word2vec';
 
-word2vec = normalizeColsL2(double(word2vec));
-nPCADims = 30;                                                                                                                                   
-[X, pcamap] = netpca(double(word2vec'), nPCADims);
-meanvec = mean(word2vec, 2);
-save('word2vec_GoogleNews_PCAmap30.mat', 'pcamap', 'meanvec');
-
-%load attr_word2vec_GoogleNews.mat
-%load synset_word2vec_GoogleNews.mat
-
-%word2vec = normalizeColsL2(double(word2vec));                                                                                                            
-
-%nPCADims = 30;                                                                                                                                   
-%[X, map] = netpca(double(word2vec'), nPCADims);
-
-%meanvec = mean(word2vec, 2);
-%word2vec = map' * (word2vec - repmat(meanvec, 1, size(word2vec, 2)));
-
-%save('attr_word2vec_GoogleNews_PCAed.mat', 'word2vec');
-%save('synset_word2vec_GoogleNews_PCAed.mat', 'word2vec');
+save('synset_word2vec_GoogleNews_PCA30.mat', 'word2vec');
+%save('attr_word2vec_GoogleNews_PCA30.mat', 'word2vec');
