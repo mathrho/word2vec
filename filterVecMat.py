1213fc95cb107297a2577b2c79a49f44148811ef
mathrho <author@corpmail.com>
2015-04-08 15:08:08 +0200

    modify wod2vec

---
+++ filterVecMat.py
@@ -0,0 +1,145 @@
+#!/datastore/zhenyang/bin/ python
+
+import sys
+import os
+import gensim, logging
+import numpy as np
+import scipy.io as sio
+from xml.etree import ElementTree
+
+def get_parentmap(tree):
+    parent_map = {}
+    for p in tree.iter():
+        for c in p:
+            if c in parent_map:
+                parent_map[c].append(p)
+                # Or raise, if you don't want to allow this.
+            else:
+                parent_map[c] = [p]
+                # Or parent_map[c] = p if you don't want to allow this
+    return parent_map
+
+
+def main():
+
+    ##############
+    imgnet_xml_file = 'structure_released.xml'
+    tree = ElementTree.parse(imgnet_xml_file)
+
+    root = tree.getroot()
+    release_data = root[0].text
+    synsets = root[1]
+
+
+    ##############
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    #pretrained_model = './vectors.bin'
+    #pretrained_model = '../freebase-vectors-skipgram1000-en.bin'
+    pretrained_model = '../GoogleNews-vectors-negative300.bin'
+    model = gensim.models.Word2Vec.load_word2vec_format(pretrained_model, binary=True)
+    #model['animal']
+    #print model.similarity('/en/dog', '/en/cat')
+    #print model.similarity('/en/dog', '/en/mountain')
+
+
+    ##############
+    parent_map = get_parentmap(tree)
+    classids = open('synsets.txt', 'r').read().splitlines()
+
+    cc = 0
+    clsid = 0
+    vec_size = 300
+    word2vec_classids = {}
+    word2vec_mat = np.zeros((len(classids), vec_size))
+    for classid in classids:
+        idc = 1
+        for target in synsets.findall(".//synset[@wnid='" + classid + "']"):
+            classnames = target.get('words').split(', ')
+            for classname in classnames:
+                #classname = '/en/' + classname.replace(' ', '_')
+                classname = classname.replace(' ', '_')
+                try:
+                    wordvec = model[classname]
+                    word2vec_mat[clsid, :] = wordvec
+                    word2vec_classids[classid] = target.get('wnid')
+                    idc = 0
+
+                except:
+                    pass
+            break
+
+        #print dir(model)
+        if idc:
+            classnames = target.get('words').split(', ')
+            for classname in classnames:
+                namewords = classname.split(' ')
+                wordvec = np.zeros(vec_size)
+                for mameword in namewords:
+                    try:
+                        wordvec = np.add(wordvec, model[mameword])
+                        idc = 0
+                    except:
+                        idc = 1
+                        break
+
+                if idc == 0:
+                    word2vec_mat[clsid, :] = wordvec
+                    word2vec_classids[classid] = target.get('wnid')
+                    break
+
+        if idc:
+            for parent in parent_map[target]:
+                if True:
+                #if parent.get('wnid') not in classids:
+
+                    classnames = parent.get('words').split(', ')
+                    for classname in classnames:
+                        #classname = '/en/' + classname.replace(' ', '_')
+                        classname = classname.replace(' ', '_')
+                        try:
+                            wordvec = model[classname]
+                            word2vec_mat[clsid, :] = wordvec
+                            word2vec_classids[classid] = parent.get('wnid')
+                            idc = 0
+                            break
+                        except:
+                            pass
+                    
+                    if idc:
+                        for classname in classnames:
+                            namewords = classname.split(' ')
+                            wordvec = np.zeros(300)
+                            for mameword in namewords:
+                                try:
+                                    wordvec = np.add(wordvec, model[mameword])
+                                    idc = 0
+                                except:
+                                    idc = 1
+                                    break
+
+                            if idc == 0:
+                                word2vec_mat[clsid, :] = wordvec
+                                word2vec_classids[classid] = parent.get('wnid')
+                                break
+
+                else:
+                    print classid + '\t' + target.get('wnid') + '\t' + parent.get('wnid') + '\n'
+
+                if idc == 0:
+                    break
+ 
+        clsid = clsid + 1
+        cc = cc + idc
+
+    #np.savetxt('synset_word2vec_GoogleNews.txt', word2vec_mat)
+    #sio.savemat('synset_word2vec_GoogleNews.mat', {'word2vec':word2vec_mat})
+    print cc
+
+    fp = open('./synset-filtered.txt', 'w')
+    for classid in classids:
+        fp.write(classid + '\t' + word2vec_classids[classid] + '\n')
+    fp.close()
+
+if __name__ == "__main__":
+    main()
