4f9bfe80c9a859dedf3de12a17b48005b6b957f8
mathrho <author@corpmail.com>
2015-06-15 18:16:46 +0200

    add train yahoo scripts

--- word2vec_yahoo_PCA.m
+++ word2vec_yahoo_PCA.m
@@ -1,22 +1,37 @@
-%load word2vec_GoogleNews.mat
-%word2vec = word2vec';
-
-%word2vec = normalizeColsL2(double(word2vec));
-%nPCADims = 30;                                                                                                                                   
-%[X, pcamap] = netpca(double(word2vec'), nPCADims);
-%meanvec = mean(word2vec, 2);
-%save('word2vec_GoogleNews_PCAmap30.mat', 'pcamap', 'meanvec');
+function word2vec_yahoo_PCA()
+%nPCADims
+%pcamap
+%meanvec
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-load word2vec_GoogleNews_PCAmap30.mat
-
-load synset_word2vec_GoogleNews.mat
-%load attr_word2vec_GoogleNews.mat
-
+load('attr_word2vec_yahoo_500d.mat')
 word2vec = word2vec';
-word2vec = normalizeColsL2(double(word2vec));
+
+nPCADims = 30;
+word2vec = normalizeColsL2(double(word2vec));                                                                                                                                 
+[X, pcamap] = netpca(double(word2vec'), nPCADims);
+meanvec = mean(word2vec, 2);
+save('attr_word2vec_yahoo_500PCA30_model.mat', 'pcamap', 'meanvec');
+
+%word2vec = word2vec';
+%word2vec = normalizeColsL2(double(word2vec));
 word2vec = pcamap' * (word2vec - repmat(meanvec, 1, size(word2vec, 2)));
 word2vec = word2vec';
+save('attr_word2vec_yahooPCA_30d.mat', 'word2vec');
 
-save('synset_word2vec_GoogleNews_PCA30.mat', 'word2vec');
-%save('attr_word2vec_GoogleNews_PCA30.mat', 'word2vec');
+clear
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+load('synset_word2vec_yahoo_500d.mat')
+word2vec = word2vec';
+
+nPCADims = 30;
+word2vec = normalizeColsL2(double(word2vec));                                                                                                                                 
+[X, pcamap] = netpca(double(word2vec'), nPCADims);
+meanvec = mean(word2vec, 2);
+save('synset_word2vec_yahoo_500PCA30_model.mat', 'pcamap', 'meanvec');
+
+%word2vec = word2vec';
+%word2vec = normalizeColsL2(double(word2vec));
+word2vec = pcamap' * (word2vec - repmat(meanvec, 1, size(word2vec, 2)));
+word2vec = word2vec';
+save('synset_word2vec_yahooPCA_30d.mat', 'word2vec');
